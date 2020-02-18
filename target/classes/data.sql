2d51d477433b77cd77b46edcdeb74fbf9465e683
Raghuveer Singh <raghuveersingh17@gmail.com>
2020-02-18 11:54:19 +0530

    Added swagger config and test data

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into person (id, first_name, last_name, age, favourite_colour) values (1, 'John', 'Wick', 29, 'red');
+insert into person_hobby values(1, 'shopping');
+insert into person_hobby values(1, 'football');
+insert into person (id, first_name, last_name, age, favourite_colour) values (2, 'Sarah', 'Raven', 54, 'blue');
+insert into person_hobby values(2, 'chess');
